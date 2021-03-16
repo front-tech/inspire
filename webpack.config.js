f5a60f899b15b2bf2677cc602f4032b6142c9332
Andrey Detelinov Andreev <adetelin@everis.com>
2021-03-16 13:20:02 +0100

    adding node_modules to docs

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,25 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/index.js',
-  mode: "development",
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'docs'),
-    clean: true,
-    library: {
-        type: 'system',
+    entry: './src/js/index.js',
+    mode: "production",
+    target: 'web',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'docs'),
+        clean: true,
+        library: {
+            type: 'system',
+        },
     },
-  },
-  plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: './index.html',
-        inject: false
-      }),
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+            inject: false
+        }),
     ],
+    resolve: {
+        modules: ['node_modules'],
+    },
 };
